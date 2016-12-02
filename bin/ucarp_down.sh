c7ab6e46ba88ad9937a5d1c794999415dc6f43ee
jonathan19mx <jons_fdz@hotmail.com>
2016-12-02 10:47:39 -0600

    Update ucarp_down.sh

--- bin/ucarp_down.sh
+++ bin/ucarp_down.sh
@@ -1,11 +1,7 @@
-#!/bin/sh
- 
-# Load variables from rc.conf
-. /etc/rc.subr
-load_rc_config ucarp
- 
-/usr/local/etc/rc.d/pgpool stop
-/sbin/ifconfig $ucarp_if -alias $ucarp_addr
-echo "Refusing to do go back online to avoid a split-brain situation."
+#!/bin/sh 
+# Load variables from rc.conf 
+./etc/rc.subr load_rc_config ucarp 
+echo "Refusing to do go back online to avoid a split-brain situation." 
 echo ""
-echo "Manually run 'pg_ha.sh slave' or 'pg_ha.sh init-slave' after ensuring the master got the latest data."
+echo "Manually run 'pg_ha.sh slave' after ensuring the master got the latest data." 
+
