819c8432153f4f0760e948cc3c7e38f75728e881
jonathan19mx <jons_fdz@hotmail.com>
2016-12-02 10:50:31 -0600

    Update ucarp_up.sh

--- bin/ucarp_up.sh
+++ bin/ucarp_up.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
- 
-# Load variables from rc.conf
-. /etc/rc.subr
-load_rc_config ucarp
- 
-/usr/local/bin/pg_ha.sh master
-/sbin/ifconfig $ucarp_if alias $ucarp_addr/32
-sleep 1
-/usr/local/etc/rc.d/pgpool start
+#!/bin/sh 
+# Load variables from rc.conf 
+./etc/rc.subr 
+load_rc_config ucarp 
+
+/usr/local/bin/pg_ha.sh master 
+/sbin/ifconfig $ucarp_if alias $ucarp_addr/32 
+
