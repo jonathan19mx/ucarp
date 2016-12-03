1e256634d99a49268c22d95623ff92154c308ad8
jonathan19mx <jons_fdz@hotmail.com>
2016-12-02 18:40:45 -0600

    Create 2ucarp_down.sh

---
+++ bin/2ucarp_down.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+# Load variables from rc.conf 
+./etc/rc.subr 
+load_rc_config ucarp 
+echo "Refusing to do go back online to avoid a split-brain situation." 
+echo "" 
+echo "Manually run 'pg_ha.sh slave' after ensuring the master got the latest data." 
